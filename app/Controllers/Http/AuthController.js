86881319ac9c176683c6c015ff23fdec73291627
KallerMG <50585470+KallerMG@users.noreply.github.com>
2019-11-21 21:04:06 -0300

    2.3

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -5,7 +5,17 @@ class AuthController {
     
     async register({ request }){
         var clas = 'aluno'
-        const data = request.only(['username', 'email', 'password', clas])
+        const data = request.only(['username', 'email', 'password', 'classe'])
+    
+         const user = await User.create(data)
+            
+        return "sucesso"
+
+    }
+
+    async registerprof({ request }){
+        
+        const data = request.only(['username', 'email', 'password', 'classe'])
     
          const user = await User.create(data)
             
@@ -14,15 +24,18 @@ class AuthController {
     }
     
     async authenticate({ request, auth }){
+        var url= {url :"inicio"};
         const{ email, password} = request.all()
         if (  request.classe = "professor"){
             const token = await auth.attempt(email, password)
 
-            return {token ,classe: request.classe}
+            return {token ,classe: request.classe, url}
         }else{
             return {token ,classe: request.classe}
-        }
-    }
+       }
+   }
+
+    
 
     async atualizar({ params, request, response }) {
         const user = await User.findOrFail(params.id);
--- start/routes.js
+++ start/routes.js
@@ -20,19 +20,25 @@ const Route = use('Route')
 
 Route.post('/criar', 'AuthController.register');
 Route.post('/authenticate', 'AuthController.authenticate');
-Route.get('/app', "AppController.index" ).middleware(["auth"]);
 Route.get('/', ({ view }) => {
   return view.render('pagina')
 })
 Route.get('/criar', ({ view }) => {
   return view.render('criar')
 })
-Route.post('/pasciente', 'PacienteController.cadastrar');
-Route.post('/anamnese', 'AnamneseController.cadastrar');
-Route.put('/user/:id', 'AuthController.atualizar');
-Route.delete('/user/:id', 'AuthController.deletar');
-Route.get('/pasciente', 'PacienteController.mostrar');
-Route.delete('/pasciente/:id', 'PacienteController.deletar');
-Route.put('/pasciente/:id', 'PacienteController.atualizar');
-Route.get('/anamnese/:id', 'AnamneseController.mostraranam');
-Route.delete('/anamnese/:id', 'AnamneseController.deletar');
+
+Route.group(() => {
+  Route.post('/criar/professor', 'AuthController.registerprof');
+  Route.put('/user/:id', 'AuthController.atualizar');
+  Route.delete('/user/:id', 'AuthController.deletar');
+  Route.post('/pasciente', 'PacienteController.cadastrar');
+  Route.get('/pasciente', 'PacienteController.mostrar');
+  Route.delete('/pasciente/:id', 'PacienteController.deletar');
+  Route.put('/pasciente/:id', 'PacienteController.atualizar');
+  Route.get('/anamnese/:id', 'AnamneseController.mostraranam');
+  Route.post('/anamnese', 'AnamneseController.cadastrar');
+  Route.delete('/anamnese/:id', 'AnamneseController.deletar');
+  Route.get('/inicio', ({ view }) => {
+    return view.render('inicio')
+  })
+}).middleware(["auth"]);
